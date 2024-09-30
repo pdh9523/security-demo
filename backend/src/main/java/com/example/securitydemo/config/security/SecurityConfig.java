532f8661db613f14b3450e27db8c158b4f401cfc
박동현 <jinee9523@gmail.com>
2024-10-01 00:43:22 +0900

    Refactor: 로그아웃 로직 변경

--- backend/src/main/java/com/example/securitydemo/config/security/SecurityConfig.java
+++ backend/src/main/java/com/example/securitydemo/config/security/SecurityConfig.java
@@ -5,11 +5,11 @@ import com.example.securitydemo.config.security.filter.TokenAuthorizationFilter;
 import com.example.securitydemo.config.security.handler.CustomAuthenticationFailureHandler;
 import com.example.securitydemo.config.security.handler.CustomAuthenticationSuccessHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,6 +17,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -39,8 +40,7 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                .requestMatchers(HttpMethod.POST,"/api/user");
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
         // 추가적인 제외 요소
     }
 
@@ -58,14 +58,20 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(tokenAuthorizationFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                // 로그아웃 시 로직. "/user/logout" 엔드포인트로 들어오는 로직을 로그아웃으로 인지해 인증 제거, 쿠키 제거, 세션 비활성화 등의 처리를 수행한다.
                 .logout(logout -> logout
                                 .logoutUrl("/user/logout")
                                 .clearAuthentication(true)
                                 .deleteCookies("JSESSIONID", "access_token", "refresh_token")
                                 .invalidateHttpSession(true)
+                                .logoutSuccessHandler((request, response, authentication) -> {
+                                    SecurityContextHolder.clearContext();
+                                    response.setStatus(HttpServletResponse.SC_OK);
+                                    response.getWriter().write("Logout successful");
+                                    response.getWriter().flush();
+                                })
                         )
                 .build();
-
     }
 
     @Bean
@@ -76,7 +82,7 @@ public class SecurityConfig {
             CustomAuthenticationFailureHandler customAuthenticationFailureHandler
     ) {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager, objectMapper);
-        // "/members/login" 엔드포인트로 들어오는 요청을 CustomAuthenticationFilter에서 처리하도록 지정한다.
+        // "/user/login" 엔드포인트로 들어오는 요청을 로그인으로 인지해 CustomAuthenticationFilter에서 처리하도록 지정한다.
         customAuthenticationFilter.setFilterProcessesUrl("/user/login");
         customAuthenticationFilter.setAuthenticationSuccessHandler(customAuthenticationSuccessHandler);    // '인증' 성공 시 해당 핸들러로 처리를 전가한다.
         customAuthenticationFilter.setAuthenticationFailureHandler(customAuthenticationFailureHandler);    // '인증' 실패 시 해당 핸들러로 처리를 전가한다.
@@ -89,6 +95,7 @@ public class SecurityConfig {
         return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
     }
 
+    // cors에 대한 설정 소스를 filterChain 밖에서 따로 설정할 수 있다.
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
