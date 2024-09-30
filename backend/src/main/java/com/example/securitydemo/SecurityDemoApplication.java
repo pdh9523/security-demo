f71647bf4e2c5076860f970336aff86bb19a849d
박동현 <jinee9523@gmail.com>
2024-10-01 00:43:09 +0900

    Feat: EnableWebSecurity적용

--- backend/src/main/java/com/example/securitydemo/SecurityDemoApplication.java
+++ backend/src/main/java/com/example/securitydemo/SecurityDemoApplication.java
@@ -3,8 +3,9 @@ package com.example.securitydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-
+@EnableWebSecurity
 @EnableJpaAuditing
 @SpringBootApplication
 public class SecurityDemoApplication {
