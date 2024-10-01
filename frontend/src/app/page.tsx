135f3e63e160d220c9963737f7d3a4b796ab8011
박동현 <jinee9523@gmail.com>
2024-10-01 14:44:27 +0900

    Feat: 메인 페이지에 로그인 여부 표시

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import {api} from "@/lib/api";
+import {useEffect, useState} from "react";
+
+
+export default function App() {
+    const [email, setEmail] = useState("");
+
+    function logout() {
+        api.post("/user/logout")
+            .then(response => console.log(response))
+    }
+
+    function myInfo() {
+        api.get("/user/my-info")
+            .then(response =>
+                setEmail(response.data.email)
+            )
+    }
+
+    useEffect(() => {
+        myInfo()
+    })
+    return (
+        <div>
+            <div>
+                {email}
+            </div>
+            <div>
+                랜딩 페이지 입니다.
+                <button
+                    onClick={() => logout()}
+                >
+                    로그아웃
+                </button>
+            </div>
+        </div>
+    )
+}
