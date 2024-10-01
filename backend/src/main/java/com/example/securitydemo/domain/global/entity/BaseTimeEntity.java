58291a5176a2c5482bd907d228d3623d753b0938
박동현 <jinee9523@gmail.com>
2024-10-02 01:55:58 +0900

    Refactor: BaseEntity 추상 클래스로 변환

--- backend/src/main/java/com/example/securitydemo/domain/global/entity/BaseTimeEntity.java
+++ backend/src/main/java/com/example/securitydemo/domain/global/entity/BaseTimeEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 // Spring Data JPA가 Auditing(자동으로 값 매핑) 기능 추가
 // 도메인을 영속성 컨텍스트에 저장하거나 조회를 수행한 후에 update를 하는 경우
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
