04e24dcffba5b37107957d92001316d2c19e274d
박동현 <jinee9523@gmail.com>
2024-10-01 15:14:16 +0900

    Comment: 함수 주석 추가

--- backend/src/main/java/com/example/securitydemo/domain/user/service/UserService.java
+++ backend/src/main/java/com/example/securitydemo/domain/user/service/UserService.java
@@ -22,7 +22,17 @@ public interface UserService {
      */
     UserResponseDto getMyInfo(User user);
 
+    /**
+     * 인증을 위한 이메일 전송
+     * @param email 인증코드를 받을 이메일
+     * @throws MessagingException 이메일이 정상적으로 발송되지 않은 경우
+     */
     void sendEmail(String email) throws MessagingException;
 
+    /**
+     * 인증코드 검증
+     * @param emailAuthCheckDto email, authCode
+     * @return 인증이 성공되었는지에 대한 여부
+     */
     Boolean verifyEmail(EmailAuthCheckDto emailAuthCheckDto);
 }
